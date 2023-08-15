140d06cbae10f26edd066a2e98e966ade9b02ee0
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-08-15 14:14:42 +0000

    feat: add github actions workflow generator

--- src/gitlab/jobs.ts
+++
@@ -1,7 +0,0 @@
-import { Job } from "https://deno.land/x/fluent_gitlab_ci@v0.3.2/mod.ts";
-
-export const test = new Job().script("go test -v ./...");
-
-export const fmt = new Job().script("go fmt ./...");
-
-export const build = new Job().script("go build");
