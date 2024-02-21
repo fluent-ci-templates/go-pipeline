861d7c65b267dbf592198d94df519ea36880cffb
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-02-21 19:32:08 +0000

    make it compatible with Dagger v0.9.11

--- ci.ts
+++ ci.ts
@@ -2,7 +2,7 @@ import {
   fmt,
   test,
   build,
-} from "https://pkg.fluentci.io/go_pipeline@v0.9.2/mod.ts";
+} from "https://pkg.fluentci.io/go_pipeline@v0.9.3/mod.ts";
 
 await fmt();
 await test();
